f8d0e9b212c14f2c00d57cd2ddb18046dc112e6b
zinatovruslan <zruslan041206@gmail.com>
2025-01-19 13:26:59 +0500

    Update modify.py

--- lab1/Strings/modify.py
+++ lab1/Strings/modify.py
@@ -1,1 +1,14 @@
+a = "Hello, World!"
+print(a.upper()) #HELLO, WORLD!
 
+a = "Hello, World!"
+print(a.lower()) #hello, world!
+
+a = " Hello, World! "
+print(a.strip()) #returns "Hello, World!"
+
+a = "Hello, World!"
+print(a.replace("H", "J")) #Jello, World!
+
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
