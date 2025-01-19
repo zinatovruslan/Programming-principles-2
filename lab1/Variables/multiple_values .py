6b166eab9a7d2eb647d9b400ca280e67347bc013
zinatovruslan <zruslan041206@gmail.com>
2025-01-19 13:22:02 +0500

    Update multiple_values .py

--- lab1/Variables/multiple_values .py
+++ lab1/Variables/multiple_values .py
@@ -1,1 +1,18 @@
+x, y, z = "Orange", "Banana", "Cherry"
+print(x) # Orange
+print(y) # Banana
+print(z) # Cherry
 
+x = y = z = "Orange"
+
+print(x) # Orange
+print(y) # Orange
+print(z) # Orange
+
+#unpacking
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x) # apple
+print(y) # banana
+print(z) # cherry
