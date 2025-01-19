ab568fdd419b2cfd6a71a5dcb7a969b39eb1e5b1
zinatovruslan <zruslan041206@gmail.com>
2025-01-19 13:24:11 +0500

    Update 1.py

--- lab1/Datatypes/1.py
+++ lab1/Datatypes/1.py
@@ -1,1 +1,65 @@
+x = str("Hello World")	#str	
+x = int(20)	#int	
+x = float(20.5)	#float	
+x = complex(1j)	#complex	
+x = list(("apple", "banana", "cherry"))	#list	
+x = tuple(("apple", "banana", "cherry"))	#tuple	
+x = range(6)#	range	
+x = dict(name="John", age=36)	#dict	
+x = set(("apple", "banana", "cherry"))	#set	
+x = frozenset(("apple", "banana", "cherry"))	#frozenset	
+x = bool(5)    #bool	
+x = bytes(5)	#bytes	
+x = bytearray(5)	#bytearray
+x = memoryview(bytes(5)) #memoryview
+x = None #NoneType
 
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+import random
+
+print(random.randrange(1, 10))
