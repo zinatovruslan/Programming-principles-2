c6948c9182653e2e6e2b618587e747f1103e8be0
zinatovruslan <zruslan041206@gmail.com>
2025-01-19 13:25:57 +0500

    Update 1.py

--- lab1/Strings/1.py
+++ lab1/Strings/1.py
@@ -1,1 +1,50 @@
+print("Hello") # Hello
+print('Hello') # Hello
 
+print("It's alright") #It's alright
+print("He is called 'Johnny'") # He is called 'Johnny'
+print('He is called "Johnny"') # He is called "Johnny"
+
+a = "Hello"
+print(a) # Hello
+
+#""" or ''' -- to comment large text or line
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+#Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.
+
+a = "Hello, World!"
+print(a[1]) # e
+
+for x in "banana":
+  print(x)
+#b
+a
+n
+a
+n
+a
+
+a = "Hello, World!"
+print(len(a)) #13
+
+txt = "The best things in life are free!"
+print("free" in txt) #True
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.") #Yes, 'free' is present.
+
+txt = "The best things in life are free!"
+print("expensive" not in txt) #True
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.") #No, 'expensive' is NOT present.
