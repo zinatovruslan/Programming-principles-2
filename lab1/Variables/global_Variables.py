c1a4e68549696b3b9b6af0aedaa4199cf692daa3
zinatovruslan <zruslan041206@gmail.com>
2025-01-19 13:23:35 +0500

    Update global_Variables.py

--- lab1/Variables/global_Variables.py
+++ lab1/Variables/global_Variables.py
@@ -1,1 +1,38 @@
+#1
+x = "awesome"
 
+def myfunc():
+  print("Python is " + x) # Python is awesome
+
+myfunc()
+
+#2
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x) # Python is fantastic
+
+myfunc()
+
+print("Python is " + x) # Python is awesome
+
+#3
+def myfunc():
+  global x  #If you use the global keyword, the variable belongs to the global scope
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x) #Python is fantastic
+
+#4
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x) #Python is fantastic
