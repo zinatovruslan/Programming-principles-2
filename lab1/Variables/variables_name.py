40782cf3785712c8613c05dbfc6e9e20f43b6259
zinatovruslan <zruslan041206@gmail.com>
2025-01-19 13:20:53 +0500

    Update variables_name.py

--- lab1/Variables/variables_name.py
+++ lab1/Variables/variables_name.py
@@ -1,1 +1,14 @@
+#legal variable names
 
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#illegal variable names
+
+2myvar = "John"
+my-var = "John"
+my var = "John"
