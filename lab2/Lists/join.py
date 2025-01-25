3699d5609b43f3a9f22e437353d42b6959a57093
zinatovruslan <zruslan041206@gmail.com>
2025-01-25 15:49:52 +0500

    Update join.py

--- lab2/Lists/join.py
+++ lab2/Lists/join.py
@@ -1,1 +1,22 @@
+#1
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
 
+list3 = list1 + list2
+print(list3) #['a', 'b', 'c', 1, 2, 3]
+
+#2
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x)
+
+print(list1) #['a', 'b', 'c', 1, 2, 3]
+
+#3
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+list1.extend(list2)
+print(list1) #['a', 'b', 'c', 1, 2, 3]
