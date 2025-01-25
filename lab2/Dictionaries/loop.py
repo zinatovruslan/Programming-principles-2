866d07f109f5ba37c7964454c432afd0909b6727
zinatovruslan <zruslan041206@gmail.com>
2025-01-25 22:58:08 +0500

    Update loop.py

--- lab2/Dictionaries/loop.py
+++ lab2/Dictionaries/loop.py
@@ -1,1 +1,59 @@
+#1
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict:
+  print(x)
+# brand
+# model
+# year
 
+#2
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict:
+  print(thisdict[x])
+# Ford
+# Mustang
+# 1964
+
+#3
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict.values():
+  print(x)
+# Ford
+# Mustang
+# 1964
+
+#4
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict.keys():
+  print(x)
+# brand
+# model
+# year
+
+#5
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x, y in thisdict.items():
+  print(x, y)
+# brand Ford
+# model Mustang
+# year 1964
