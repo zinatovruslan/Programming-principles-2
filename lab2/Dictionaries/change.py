3467e5bdaf62d67158d410e611d1b5584c5bea82
zinatovruslan <zruslan041206@gmail.com>
2025-01-25 22:52:44 +0500

    Update change.py

--- lab2/Dictionaries/change.py
+++ lab2/Dictionaries/change.py
@@ -1,1 +1,20 @@
+#1
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
 
+thisdict["year"] = 2018
+
+print(thisdict) #{'brand': 'Ford', 'model': 'Mustang', 'year': 2018}
+
+#2
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"year": 2020})
+
+print(thisdict) #{'brand': 'Ford', 'model': 'Mustang', 'year': 2020}
