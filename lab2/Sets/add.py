89a6fd82e7e93bcba6e10844854882a63c7712d4
zinatovruslan <zruslan041206@gmail.com>
2025-01-25 21:01:12 +0500

    Update add.py

--- lab2/Sets/add.py
+++ lab2/Sets/add.py
@@ -1,1 +1,22 @@
+#1
+thisset = {"apple", "banana", "cherry"}
 
+thisset.add("orange")
+
+print(thisset) #{'cherry', 'orange', 'apple', 'banana'}
+
+#2
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+
+thisset.update(tropical)
+
+print(thisset) #{'apple', 'mango', 'cherry', 'pineapple', 'banana', 'papaya'}
+
+#3
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+
+thisset.update(mylist)
+
+print(thisset) #{'banana', 'cherry', 'apple', 'orange', 'kiwi'}
