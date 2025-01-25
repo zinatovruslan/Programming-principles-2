06e0aa5d5d62874f3a15efc807a2c0f33ca03b39
zinatovruslan <zruslan041206@gmail.com>
2025-01-25 15:28:43 +0500

    Update remove.py

--- lab2/Lists/remove.py
+++ lab2/Lists/remove.py
@@ -1,1 +1,34 @@
+#1
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist) #['apple', 'cherry']
 
+#2
+thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+thislist.remove("banana")
+print(thislist) #['apple', 'cherry', 'banana', 'kiwi']
+
+#3
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist) #['apple', 'cherry']
+
+#4
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist) #['apple', 'banana']
+
+#5
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist) #['banana', 'cherry']
+
+#6
+thislist = ["apple", "banana", "cherry"]
+del thislist 
+print(thislist) #we didn't said index so the whole list is delete and it caused the error
+
+#7
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist) #[]
