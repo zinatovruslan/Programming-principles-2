a90a3bfcc9ec8c8c0e35c0c0ca9959c1186d0e4c
zinatovruslan <zruslan041206@gmail.com>
2025-01-25 22:59:10 +0500

    Update copy.py

--- lab2/Dictionaries/copy.py
+++ lab2/Dictionaries/copy.py
@@ -1,1 +1,17 @@
+#1
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = thisdict.copy()
+print(mydict) #{'brand': 'Ford', 'model': 'Mustang', 'year': 1964}
 
+#2
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = dict(thisdict)
+print(mydict) #{'brand': 'Ford', 'model': 'Mustang', 'year': 1964}
