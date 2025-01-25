220e1728afe63a58df425f6678a2ee77d827772a
zinatovruslan <zruslan041206@gmail.com>
2025-01-25 20:47:52 +0500

    Update loop.py

--- lab2/Tuples/loop.py
+++ lab2/Tuples/loop.py
@@ -1,1 +1,25 @@
+#1
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+  print(x) 
+# apple
+# banana
+# cherry
 
+#2
+thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+  print(thistuple[i])
+# apple
+# banana
+# cherry
+
+#3
+thistuple = ("apple", "banana", "cherry")
+i = 0
+while i < len(thistuple):
+  print(thistuple[i])
+  i = i + 1
+# apple
+# banana
+# cherry
