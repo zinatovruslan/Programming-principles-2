38ab28a079f8195ee02bf6dc93ed1dcbf29161c8
zinatovruslan <zruslan041206@gmail.com>
2025-01-25 15:53:11 +0500

    Update methods.py

--- lab2/Lists/methods.py
+++ lab2/Lists/methods.py
@@ -1,1 +1,22 @@
+#index
+#1
+fruits = ['apple', 'banana', 'cherry']
+x = fruits.index("cherry")
+print(x) #2
 
+#2
+fruits = [4, 55, 64, 32, 16, 32]
+x = fruits.index(32)
+print(x) #3
+
+
+#count
+#1
+fruits = ["apple", "banana", "cherry"]
+x = fruits.count("cherry")
+print(x) #1
+
+#2
+points = [1, 4, 2, 9, 7, 8, 9, 3, 1]
+x = points.count(9)
+print(x) #2
