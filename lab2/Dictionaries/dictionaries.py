dd41ccc7770faeb998978acc1e28d780e56daf65
zinatovruslan <zruslan041206@gmail.com>
2025-01-25 22:47:01 +0500

    Update dictionaries.py

--- lab2/Dictionaries/dictionaries.py
+++ lab2/Dictionaries/dictionaries.py
@@ -1,1 +1,56 @@
+#1
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict) #{'brand': 'Ford', 'model': 'Mustang', 'year': 1964}
 
+#2
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"]) #Ford
+
+#3
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict) #{'brand': 'Ford', 'model': 'Mustang', 'year': 2020}
+
+#4
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(len(thisdict)) #3
+
+#5
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+
+print(thisdict) #{'brand': 'Ford', 'electric': False, 'year': 1964, 'colors': ['red', 'white', 'blue']}
+
+#6
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict)) #<class 'dict'>
+
+#7
+thisdict = dict(name = "John", age = 36, country = "Norway")
+
+print(thisdict)  #{'name': 'John', 'age': 36, 'country': 'Norway'}
