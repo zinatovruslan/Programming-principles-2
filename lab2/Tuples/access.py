0242570aa482302ca5617199495d751cd70991c7
zinatovruslan <zruslan041206@gmail.com>
2025-01-25 20:40:10 +0500

    Update access.py

--- lab2/Tuples/access.py
+++ lab2/Tuples/access.py
@@ -1,1 +1,28 @@
+#1
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1]) #banana
 
+#2
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1]) #cherry
+
+#3
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5]) #('cherry', 'orange', 'kiwi')
+
+#4
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[:4]) #('apple', 'banana', 'cherry', 'orange')
+
+#5
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:]) #('cherry', 'orange', 'kiwi', 'melon', 'mango')
+
+#6
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1]) #('orange', 'kiwi', 'melon')
+
+#7
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple") #Yes, 'apple' is in the fruits tuple
