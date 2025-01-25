f01c700dfaabbcfa5cf7c374c8e4b167a4980e90
zinatovruslan <zruslan041206@gmail.com>
2025-01-25 21:04:05 +0500

    Update remove.py

--- lab2/Sets/remove.py
+++ lab2/Sets/remove.py
@@ -1,1 +1,25 @@
+#1
+thisset = {"apple", "banana", "cherry"}
+thisset.remove("banana")
+print(thisset) #{'apple', 'cherry'}
 
+#2
+thisset = {"apple", "banana", "cherry"}
+thisset.discard("banana")
+print(thisset) #{'apple', 'cherry'}
+
+#3
+thisset = {"apple", "banana", "cherry"}
+x = thisset.pop()
+print(x) #removed item -- cherry
+print(thisset) #the set after removal -- {'apple', 'banana'}
+
+#4
+thisset = {"apple", "banana", "cherry"}
+thisset.clear()
+print(thisset) #set()
+
+#5
+thisset = {"apple", "banana", "cherry"}
+del thisset
+print(thisset) #this will raise an error because the set no longer exists
