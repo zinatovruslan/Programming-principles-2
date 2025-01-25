ae5d20b00bd0050a56ab97ba4272a017d420dfad
zinatovruslan <zruslan041206@gmail.com>
2025-01-25 20:49:18 +0500

    Update join.py

--- lab2/Tuples/join.py
+++ lab2/Tuples/join.py
@@ -1,1 +1,12 @@
+#1
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
 
+tuple3 = tuple1 + tuple2
+print(tuple3) #('a', 'b', 'c', 1, 2, 3)
+
+#2
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+
+print(mytuple) #('apple', 'banana', 'cherry', 'apple', 'banana', 'cherry')
