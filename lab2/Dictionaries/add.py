058507bb9a0375bddb22a68462d8e9d60f2847ee
zinatovruslan <zruslan041206@gmail.com>
2025-01-25 22:54:00 +0500

    Update add.py

--- lab2/Dictionaries/add.py
+++ lab2/Dictionaries/add.py
@@ -1,1 +1,18 @@
+#1
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["color"] = "red"
+print(thisdict) #{'brand': 'Ford', 'model': 'Mustang', 'year': 1964, 'color': 'red'}
 
+#2
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"color": "red"})
+
+print(thisdict) #{'brand': 'Ford', 'model': 'Mustang', 'year': 1964, 'color': 'red'}
