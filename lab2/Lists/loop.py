1dcfc00383c16eb1446b41fac3813f5e981f6952
zinatovruslan <zruslan041206@gmail.com>
2025-01-25 15:32:03 +0500

    Update loop.py

--- lab2/Lists/loop.py
+++ lab2/Lists/loop.py
@@ -1,1 +1,32 @@
+#1
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x) 
+# apple
+# banana
+# cherry
 
+#2
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i]) 
+# apple
+# banana
+# cherry
+
+#3
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i]) 
+  i = i + 1
+# apple
+# banana
+# cherry
+
+#4
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist] 
+# apple
+# banana
+# cherry
