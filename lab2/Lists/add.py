6bddf5f40e82fbf8f8705522d81048c4d847c920
zinatovruslan <zruslan041206@gmail.com>
2025-01-25 15:23:46 +0500

    Update add.py

--- lab2/Lists/add.py
+++ lab2/Lists/add.py
@@ -1,1 +1,21 @@
+#1
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist) #['apple', 'banana', 'cherry', 'orange']
 
+#2
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist) #['apple', 'orange', 'banana', 'cherry']
+
+#3
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist) #['apple', 'banana', 'cherry', 'mango', 'pineapple', 'papaya']
+
+#4
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist) #['apple', 'banana', 'cherry', 'kiwi', 'orange']
