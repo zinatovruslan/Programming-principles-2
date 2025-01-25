54f77ceeee9dd650d5aee37731930399b7763460
zinatovruslan <zruslan041206@gmail.com>
2025-01-25 20:42:20 +0500

    Update update.py

--- lab2/Tuples/update.py
+++ lab2/Tuples/update.py
@@ -1,1 +1,35 @@
+#1
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
 
+print(x) #("apple", "kiwi", "cherry")
+
+#2
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+
+print(thistuple) #('apple', 'banana', 'cherry', 'orange')
+
+#3
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+
+print(thistuple) #('apple', 'banana', 'cherry', 'orange')
+
+#4
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
+
+print(thistuple) #('banana', 'cherry')
+
+#5
+thistuple = ("apple", "banana", "cherry")
+del thistuple
+print(thistuple) #this will raise an error because the tuple no longer exists
