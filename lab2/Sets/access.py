8b69ccae5ef2994818b9f7525dad82ab973a0dea
zinatovruslan <zruslan041206@gmail.com>
2025-01-25 20:59:05 +0500

    Update access.py

--- lab2/Sets/access.py
+++ lab2/Sets/access.py
@@ -1,1 +1,16 @@
+#1
+thisset = {"apple", "banana", "cherry"}
 
+for x in thisset:
+  print(x) 
+# cherry
+# banana
+# apple
+
+#2
+thisset = {"apple", "banana", "cherry"}
+print("banana" in thisset) #True
+
+#3
+thisset = {"apple", "banana", "cherry"}
+print("banana" not in thisset) #False
