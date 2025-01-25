a398b4cbf39dd7122b3ad2588efd9841fe826d47
zinatovruslan <zruslan041206@gmail.com>
2025-01-25 20:46:12 +0500

    Update unpack.py

--- lab2/Tuples/unpack.py
+++ lab2/Tuples/unpack.py
@@ -1,1 +1,30 @@
+#1
+fruits = ("apple", "banana", "cherry")
+print(fruits) #('apple', 'banana', 'cherry')
 
+#2
+fruits = ("apple", "banana", "cherry")
+
+(green, yellow, red) = fruits
+
+print(green) #apple
+print(yellow) #banana
+print(red) #cherry
+
+#3
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+(green, yellow, *red) = fruits
+
+print(green) #apple
+print(yellow) #banana
+print(red) #['cherry', 'strawberry', 'raspberry']
+
+#4
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+(green, *tropic, red) = fruits
+
+print(green) #apple
+print(tropic) #['mango', 'papaya', 'pineapple']
+print(red) #cherry
