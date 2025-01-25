188129f5702520871944e4384b858258e7109b48
zinatovruslan <zruslan041206@gmail.com>
2025-01-25 15:46:39 +0500

    Update sort.py

--- lab2/Lists/sort.py
+++ lab2/Lists/sort.py
@@ -1,1 +1,41 @@
+#1
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist) #['banana', 'kiwi', 'mango', 'orange', 'pineapple']
 
+#2
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist) #[23, 50, 65, 82, 100]
+
+#3
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist) #['pineapple', 'orange', 'mango', 'kiwi', 'banana']
+
+#4
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse = True)
+print(thislist) #[100, 82, 65, 50, 23]
+
+#5
+def myfunc(n):
+  return abs(n - 50)
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist) #[50, 65, 23, 82, 100]
+
+#6
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort()
+print(thislist) #['Kiwi', 'Orange', 'banana', 'cherry']
+
+#7
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort(key = str.lower)
+print(thislist) #['banana', 'cherry', 'Kiwi', 'Orange']
+
+#8
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.reverse()
+print(thislist) #['cherry', 'Kiwi', 'Orange', 'banana']
