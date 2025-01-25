6002a040571cdccd39d96636ef9d017737ff71a0
zinatovruslan <zruslan041206@gmail.com>
2025-01-25 15:48:20 +0500

    Update copy.py

--- lab2/Lists/copy.py
+++ lab2/Lists/copy.py
@@ -1,1 +1,14 @@
+#1
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist) #['apple', 'banana', 'cherry']
 
+#2
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist) #['apple', 'banana', 'cherry']
+
+#3
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist[:]
+print(mylist) #['apple', 'banana', 'cherry']
