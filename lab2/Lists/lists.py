8b491b485fa731b6e2c171603114bb9fcfe7cf4a
zinatovruslan <zruslan041206@gmail.com>
2025-01-25 15:13:54 +0500

    Update lists.py

--- lab2/Lists/lists.py
+++ lab2/Lists/lists.py
@@ -1,1 +1,28 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist) #['apple', 'banana', 'cherry']
 
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist)) #3
+
+#1
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+print(list1) #['apple', 'banana', 'cherry']
+print(list2) #[1, 5, 7, 9, 3]
+print(list3) #[True, False, False]
+
+#2
+list1 = ["abc", 34, True, 40, "male"]
+
+print(list1) #['abc', 34, True, 40, 'male']
+
+#3
+mylist = ["apple", "banana", "cherry"]
+
+print(type(mylist)) #<class 'list'>
+
+#4
+thislist = list(("apple", "banana", "cherry"))
+print(thislist) #['apple', 'banana', 'cherry']
