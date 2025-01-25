81114703a2a1861dc98c6042f23d9fc587164430
zinatovruslan <zruslan041206@gmail.com>
2025-01-25 22:55:27 +0500

    Update remove.py

--- lab2/Dictionaries/remove.py
+++ lab2/Dictionaries/remove.py
@@ -1,1 +1,44 @@
+#1
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.pop("model")
+print(thisdict) #{'brand': 'Ford', 'year': 1964}
 
+#2
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.popitem()
+print(thisdict) #{'brand': 'Ford', 'model': 'Mustang'}
+
+#3
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict["model"]
+print(thisdict) #{'brand': 'Ford', 'year': 1964}
+
+#4
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict
+print(thisdict) #this will cause an error because "thisdict" no longer exists.
+
+#5
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.clear()
+print(thisdict) #{}
